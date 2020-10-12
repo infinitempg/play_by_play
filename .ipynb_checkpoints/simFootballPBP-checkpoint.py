b3f2784adb7126449a6bc2bd1daac13ae6ba8685
infinitempg <mikemakmur@gmail.com>
2020-10-12 17:22:27 -0500

    points

--- .ipynb_checkpoints/simFootballPBP-checkpoint.py
+++ .ipynb_checkpoints/simFootballPBP-checkpoint.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 import re
-from tqdm import tqdm_notebook as tqdm
+# from tqdm import tqdm_notebook as tqdm
 from urllib.request import urlopen
+import os
 
 requestHead = {"User-Agent": "Chrome/47.0.2526.111"}
 
@@ -24,7 +25,10 @@ def getSeasonIDs(num,online = True,prefix = None,post=True):
         with open(prefix+'/GameResults.html') as f:
             soup = BeautifulSoup(f,'html.parser')
     
-    if num > 21:
+    if num > 24:
+        preseason = 28
+        postseason = 7
+    elif num > 21:
         preseason=24
         postseason=7
     elif num > 15:
@@ -36,6 +40,33 @@ def getSeasonIDs(num,online = True,prefix = None,post=True):
     else:
         preseason = 12
         postseason= 3
+        
+    if num < 2:
+        gpwk = 3
+    elif num <= 15:
+        gpwk = 4
+    elif num <= 21:
+        gpwk = 5
+    elif num <= 24:
+        gpwk = 6
+    else:
+        gpwk = 7
+        
+    if num <= 15:
+        wks = 14
+    elif num <= 22:
+        wks = 13
+    else:
+        wks = 16
+        
+    wkList = np.repeat(range(1,wks+1),gpwk)
+    if num <= 15:
+        wkList = np.append(wkList,np.repeat(wks+1,postseason))
+        wkList[-1] = wks+2
+    else:
+        wkList = np.append(wkList,np.repeat(wks+1,4))
+        wkList = np.append(wkList,np.repeat(wks+2,3))
+        wkList[-1] = wks+3
     
     pbplist = soup.find_all('a',href=re.compile('Logs'))
     pbpURLs = [p.get('href') for p in pbplist]
@@ -45,7 +76,7 @@ def getSeasonIDs(num,online = True,prefix = None,post=True):
         idList = [p[5:].strip('.html') for p in pbpURLs[preseason:]]
     else:
         idList = [p[5:].strip('.html') for p in pbpURLs[preseason:-postseason]]
-    return idList
+    return idList, dict(zip(idList,wkList))
 
 def getTeams(S,teamID):
     if S < 5:
@@ -57,9 +88,13 @@ def getTeams(S,teamID):
     elif S <= 21:
         teamIDs = {'1':'BAL','2':'YKW','3':'COL','4':'AZ','5':'OCO',
                    '6':'SJS','7':'PHI','8':'NO','9':'CHI','10':'AUS'}
-    else:
+    elif S <= 24:
         teamIDs = {'1':'BAL','2':'YKW','3':'COL','4':'AZ','5':'OCO','6':'SJS',
                    '7':'PHI','8':'NO','9':'CHI','10':'AUS','11':'SAR','12':'HON'}
+    else:
+        teamIDs = {'1':'BAL','2':'YKW','3':'COL','4':'AZ','5':'OCO','6':'SJS',
+                   '7':'PHI','8':'NO','9':'CHI','10':'AUS','11':'SAR','12':'HON',
+                   '13':'BER','14':'NY'}
     return teamIDs[teamID]
 
 def dist2goal(team,side,yard):
@@ -108,7 +143,7 @@ def puntPoss(play,teamPoss,awayTeam,homeTeam):
     else:
         return teamPoss
 
-def getGameData(S,gameID):
+def getGameData(S,gameID,idDict):
     if S < 10:
         strnum = '0' + str(S)
     elif S >= 10:
@@ -160,6 +195,7 @@ def getGameData(S,gameID):
     pbpDF.columns = ['teamID','Q','time','totTime','down','distance','side','yard','play']
     pbpDF['gameID'] = gameID
     pbpDF['S'] = S
+    pbpDF['W'] = idDict[gameID]
         
     teamList = list(pbpDF['side'].unique())
     teamList = [t for t in teamList if t != '']
@@ -204,3 +240,151 @@ def getGameData(S,gameID):
         pbpDF['homeScore'].iloc[-1] = boxDF['homeScore'].iloc[-1]
     
     return pbpDF
+
+def posStatDF(S,gameID,boxList,index,homeTeam,awayTeam,name):
+    away = boxList[index].iloc[1:]
+    away.columns = ['Player'] + list(boxList[index].iloc[0][1:])
+    away['Team'] = awayTeam
+    
+    home = boxList[index+1].iloc[1:]
+    home.columns = ['Player'] + list(boxList[index].iloc[0][1:])
+    home['Team'] = homeTeam
+    
+    cols = ['Team'] + list(home.columns[:-1])
+    stats = pd.concat([home,away])
+    stats = stats[cols]
+    stats = stats.rename(columns={'TD':'%s_TD'%name})
+    stats['Player'] = stats['Player'].str.replace(r" \(.*?\)","").str.replace(r"\(.*?\) ","")
+    stats = stats.set_index('Player')
+    
+    if index == 8:
+        stats['Cp/At'] = stats['Cp/At'].str.split('/').apply(lambda x: [int(i) for i in x]) 
+    elif index == 14:
+        stats['FG < 20'] = stats['FG < 20'].str.split('/').apply(lambda x: [int(i) for i in x]) 
+        stats['20-29'] = stats['20-29'].str.split('/').apply(lambda x: [int(i) for i in x]) 
+        stats['30-39'] = stats['30-39'].str.split('/').apply(lambda x: [int(i) for i in x]) 
+        stats['40-49'] = stats['40-49'].str.split('/').apply(lambda x: [int(i) for i in x]) 
+        stats['50+'] = stats['50+'].str.split('/').apply(lambda x: [int(i) for i in x]) 
+    elif index == 18:
+        stats = stats.iloc[:,:9]
+        stats.columns = ['Team','KR','KRYds','PRYds','KRLng','PRLng','KR_TD','PR_TD','PR']
+    elif index == 20:
+        stats['Blk P/XP/FG'] = stats['Blk P/XP/FG'].str.split('/').apply(lambda x: [int(i) for i in x]) 
+        
+    stats.to_csv('Boxscores/S%s/%s/%sStats.csv'%(S,gameID,name))
+    return stats
+
+def getGameBox(S,gameID):
+    if not os.path.exists('Boxscores/S%s/%s'%(S,gameID)):
+        os.makedirs('Boxscores/S%s/%s'%(S,gameID))
+        
+    if S < 10:
+        strnum = '0' + str(S)
+    else:
+        strnum = str(S)
+    
+    sIDList, sIDDict = getSeasonIDs(S)
+    
+    if S < 24:
+        leagueName = 'NSFL'
+    else:
+        leagueName = 'ISFL'
+    
+    boxList = pd.read_html('https://index.sim-football.com/%sS%s/Boxscores/%s.html'%(leagueName,strnum,gameID))
+    
+    # BOX SCORE 
+    boxScore = boxList[2].iloc[:-1,:-1]
+    boxScore.to_csv('Boxscores/S%s/%s/Boxscore.csv'%(S,gameID))
+    
+    # SCORING SUMMARY
+    scoreSum = boxList[5]
+    homeTeam = scoreSum['Scoring Summary.5'].iloc[0]
+    awayTeam = scoreSum['Scoring Summary.4'].iloc[0]
+    scoreSum = scoreSum.rename(columns = {'Scoring Summary':'Quarter','Scoring Summary.1':'Score Type','Scoring Summary.2':'Time Remaining',
+                                          'Scoring Summary.3':'Play','Scoring Summary.4':awayTeam,'Scoring Summary.5':homeTeam})
+
+    isnull = scoreSum['Quarter'].isnull()
+    partitions = (isnull != isnull.shift()).cumsum()
+
+    gb = scoreSum[isnull].groupby(partitions)
+
+    q = 0
+    offset = 0
+    qD = {}
+    for i in range(1,len(isnull)):
+        if isnull[i] == True and isnull[i-1] == False:
+            q += 1
+            qD[q] = (q-offset)*2
+        elif isnull[i] == False and isnull[i-1] == False:
+            q += 1
+            offset += 1
+
+    qL = []
+    for i in qD.keys():
+    #     print(i)
+        p1 = gb.get_group(qD[i]).iloc[:,1:]
+        p1['Q'] = i
+        qL.append(p1)
+    scoring = pd.concat(qL)
+    cols = ['Q'] + list(scoring.columns[:-1])
+    scoring = scoring[cols]
+    scoring.to_csv('Boxscores/S%s/%s/Scoring.csv'%(S,gameID))
+    
+    # TEAM STATS
+    teamStats = boxList[6].iloc[1:]
+    teamStats.columns = ['Stat'] + list(boxList[6].iloc[0][1:])
+
+    teamStatsDF = teamStats.copy()
+
+    def pct(teamStats,iloc1,iloc2):
+        num = int(teamStats.iloc[iloc1,iloc2].split('/')[0])
+        denom = int(teamStats.iloc[iloc1,iloc2].split('/')[1])
+        if denom != 0:
+            return num/denom
+        else:
+            return 0
+
+    for op in [1,2,5]:
+        teamStatsDF.iloc[op,1] = pct(teamStats,op,1)
+        teamStatsDF.iloc[op,2] = pct(teamStats,op,2)
+
+    aComp = int(teamStats.iloc[5,1].split('/')[0])
+    aAtt = int(teamStats.iloc[5,1].split('/')[1])
+    hComp = int(teamStats.iloc[5,2].split('/')[0])
+    hAtt = int(teamStats.iloc[5,2].split('/')[1])
+    aPen = int(teamStats.iloc[10,1].split('-')[0])
+    aPenY = int(teamStats.iloc[10,1].split('-')[1])
+    hPen = int(teamStats.iloc[10,2].split('-')[0])
+    hPenY = int(teamStats.iloc[10,2].split('-')[1])
+    aFum = int(teamStats.iloc[12,1].split(' (')[0])
+    aFumL = int(teamStats.iloc[12,1].split(' (')[1][:-1])
+    hFum = int(teamStats.iloc[12,2].split(' (')[0])
+    hFumL = int(teamStats.iloc[12,2].split(' (')[1][:-1])
+
+    arr = [['Completions',aComp,hComp],
+           ['Attempts',aAtt,hAtt],
+           ['Penalties',aPen,hPen],
+           ['Penalty Yards',aPenY,hPenY],
+           ['Fumbles',aFum,hFum],
+           ['Fumbles Lost',aFumL,hFumL]]
+
+    extraDF = pd.DataFrame(arr, columns = teamStats.columns)
+    extraDF
+
+    teamStatsDF2 = pd.concat([teamStatsDF.iloc[:5],extraDF.iloc[:2],teamStatsDF.iloc[6:10],
+                              extraDF.iloc[2:4],teamStatsDF.iloc[11:12],extraDF[4:],teamStatsDF.iloc[13:]])
+    teamStatsDF2[awayTeam] = pd.to_numeric(teamStatsDF2[awayTeam],errors='ignore')
+    teamStatsDF2[homeTeam] = pd.to_numeric(teamStatsDF2[homeTeam],errors='ignore')
+    teamStatsDF2.to_csv('Boxscores/S%s/%s/TeamStats.csv'%(S,gameID))
+    
+    # POSITION STATS
+    passStats = posStatDF(S,gameID,boxList,8,homeTeam,awayTeam,"pass")
+    rushStats = posStatDF(S,gameID,boxList,10,homeTeam,awayTeam,'rush')
+    recStats = posStatDF(S,gameID,boxList,12,homeTeam,awayTeam,'rec')
+    kickStats = posStatDF(S,gameID,boxList,14,homeTeam,awayTeam,'kick')
+    puntStats = posStatDF(S,gameID,boxList,16,homeTeam,awayTeam,'punt')
+    specStats = posStatDF(S,gameID,boxList,18,homeTeam,awayTeam,'spec')
+    defStats = posStatDF(S,gameID,boxList,20,homeTeam,awayTeam,'def')
+    othStats = posStatDF(S,gameID,boxList,22,homeTeam,awayTeam,'oth')
+    
+    return
